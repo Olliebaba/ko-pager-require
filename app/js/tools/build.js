e0a6e7b0b7caf24657245f3814838676680cf5b3
nemo <nemo.burian@gmail.com>
2016-11-04 21:51:54 -0700

    Updating bundle.js for correct paths.

--- app/js/tools/build.js
+++ app/js/tools/build.js
@@ -1,6 +1,6 @@
 {
     "appDir": "../../",
-    "baseUrl": "js",
+    "baseUrl": ".",
     "dir": "../../../dist",
     //Comment out the optimize line if you want
     //the code minified by UglifyJS
@@ -13,7 +13,7 @@
         //Optimize the application files. jQuery and the plugins will be included
         //automatically, as they are dependencies of app.js.
         {
-            "name": "../main"
+            "name": "main"
         }
     ]
 }
